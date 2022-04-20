348e2bc6c2f4bb19a8b010bc135604a6251cc5f8
nakahi1123 <dc.h.nakanowatari@gmail.com>
2022-04-20 11:21:10 +0900

    'コマンドライン引数を受け取る'

--- lgtm/core.py
+++ lgtm/core.py
@@ -4,19 +4,15 @@ import click
 # from lgtm.image_source import get_image
 
 @click.command()
-# @click.option('--message', '-m', default='LGTM',
-#               show_default=True, help='画像に乗せる文字列')
-# @click.argument('keyword')
-#def cli(keyword, message):
-def cli():
+@click.option('--message', '-m', default='LGTM',
+              show_default=True, help='画像に乗せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
     """LGTM画像生成ツール"""
-#    lgtm(keyword, message)
-    lgtm()
-#    lgtm(keyword, message)
+    lgtm(keyword, message)
     click.echo('lgtm')
 
-#def lgtm(keyword, message):
-def lgtm():
+def lgtm(keyword, message):
 #     with get_image(keyword) as fp:
 #         save_with_message(fp, message)
     pass
