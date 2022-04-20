63c96564b6574364d95c7aea6893ffb614fc7880
nakahi1123 <dc.h.nakanowatari@gmail.com>
2022-04-20 11:43:37 +0900

    '処理追加'

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,7 +1,7 @@
 import click
 
-# from lgtm.drawer import save_with_message
-# from lgtm.image_source import get_image
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
 
 @click.command()
 @click.option('--message', '-m', default='LGTM',
@@ -10,9 +10,7 @@ import click
 def cli(keyword, message):
     """LGTM画像生成ツール"""
     lgtm(keyword, message)
-    click.echo('lgtm')
 
 def lgtm(keyword, message):
-#     with get_image(keyword) as fp:
-#         save_with_message(fp, message)
-    pass
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
